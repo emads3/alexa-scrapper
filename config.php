ce0c1392efd9afc9ce0d81240add0e0d99e63352
Emad Saeed <e@emads3.com>
2020-08-04 11:17:17 +0200

    setup eloquent and phinx for DB operations

--- config.php
+++ config.php
@@ -1,28 +1,9 @@
 <?php
-use Illuminate\Database\Capsule\Manager as Capsule;
 
-$capsule = new Capsule;
-
-$capsule->addConnection([
-	'driver'   => 'mysql',
-	'host'     => 'localhost',
-	'database' => 'alexa',
-	'username' => 'root',
-	'password' => 'root',
-	'charset'   => 'utf8',
-	'collation' => 'utf8_unicode_ci',
-	'prefix'   => '',
-]);
-
-
-// Set the event dispatcher used by Eloquent models... (optional)
-use Illuminate\Events\Dispatcher;
-use Illuminate\Container\Container;
-$capsule->setEventDispatcher(new Dispatcher(new Container));
-
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
+define('DB_DRIVER', 'mysql');
+define('DB_PREFIX', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'alexa');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_PORT', 3306);
---
+++ database.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Container\Container;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+
+$capsule = new Capsule;
+
+
+$capsule->addConnection([
+	'driver' => DB_DRIVER,
+	'host' => DB_HOST,
+	'port' => DB_PORT,
+	'database' => DB_NAME,
+	'username' => DB_USER,
+	'password' => DB_PASSWORD,
+	'charset' => 'utf8',
+	'collation' => 'utf8_unicode_ci',
+	'prefix' => '',
+]);
+
+
+// Set the event dispatcher used by Eloquent models... (optional)
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
