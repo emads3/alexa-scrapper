d3eaca384a9726497f1b6743f44d6bf4335d5aa3
Emad Saeed <e@emads3.com>
2020-08-06 15:10:06 +0200

    remove migrations

--- src/database.php
+++ src/database.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Container\Container;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Events\Dispatcher;
 
@@ -8,20 +7,16 @@ $capsule = new Capsule;
 
 
 $capsule->addConnection([
-	'driver' => DB_DRIVER,
-	'host' => DB_HOST,
-	'port' => DB_PORT,
-	'database' => DB_NAME,
-	'username' => DB_USER,
-	'password' => DB_PASSWORD,
-	'charset' => DB_CHARSET,
+	'driver'    => DB_DRIVER,
+	'host'      => DB_HOST,
+	'port'      => DB_PORT,
+	'database'  => DB_NAME,
+	'username'  => DB_USER,
+	'password'  => DB_PASSWORD,
+	'charset'   => DB_CHARSET,
 	'collation' => DB_COLLATION,
-	'prefix' => DB_PREFIX,
+	'prefix'    => DB_PREFIX,
 ]);
-
-
-// Set the event dispatcher used by Eloquent models... (optional)
-$capsule->setEventDispatcher(new Dispatcher(new Container));
 
 // Make this Capsule instance available globally via static methods... (optional)
 $capsule->setAsGlobal();
